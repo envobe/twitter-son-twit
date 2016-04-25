28f9eedeac7b44a5b0b3e2f7178d0bc7f1fd0978
envobe <envobe@hotmail.com>
2016-04-25 09:31:13 +0300

    Added files via upload

---
+++ index.php
@@ -0,0 +1,53 @@
+<style>
+.twitter-bubble {
+	position: relative;
+	width: 200px;
+	padding: 19px;
+	background: #96DDFF;
+	-webkit-border-radius: 10px;
+	-moz-border-radius: 10px;
+	border-radius: 10px;
+	color: #474747;
+	display: inline-block;
+}
+
+.twitter-bubble:after 
+{
+	content: '';
+	position: absolute;
+	border-style: solid;
+	border-width: 15px 11px 0;
+	border-color: #96DDFF transparent;
+	display: block;
+	width: 0;
+	z-index: 1;
+	bottom: -15px;
+	left: 111px;
+}
+</style>
+<?php
+
+include_once('twitteroauth/twitteroauth.php');
+
+$twitter_customer_key           = 'zw3ZAa8dSh6AGuVQe0z75uvNS';
+$twitter_customer_secret        = 'IP92YHnj96fDWWrIsU6xyaaOKWQBWSp4EtzfT4y4oYCtWavAod';
+$twitter_access_token           = '24934210-rSYeB5BqlOGBtz2UnAKNTmwsAfkxVZ4t0DyU60YK2';
+$twitter_access_token_secret    = 'A5ZElBSdU7hyf06jm1e5DHbI3nLev5da4fmuPoQANViLt';
+
+$connection = new TwitterOAuth($twitter_customer_key, $twitter_customer_secret, $twitter_access_token, $twitter_access_token_secret);
+
+$my_tweets = $connection->get('statuses/user_timeline', array('screen_name' => 'envobe', 'count' => 1));
+
+echo '<div class="twitter-bubble">';
+if(isset($my_tweets->errors))
+{           
+    echo 'Error :'. $my_tweets->errors[0]->code. ' - '. $my_tweets->errors[0]->message;
+}else{
+    echo makeClickableLinks($my_tweets[0]->text);
+}
+echo '</div>';
+
+function makeClickableLinks($s) {
+  return preg_replace('@(https?://([-\w\.]+[-\w])+(:\d+)?(/([\w/_\.#-]*(\?\S+)?[^\.\s])?)?)@', '<a target="blank" rel="nofollow" href="$1" target="_blank">$1</a>', $s);
+}
+?>
